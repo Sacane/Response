de76315b67502be23390d267f6ad4d525d5a50e2
Sacane <johan.ramaroson@gmail.com>
2023-07-21 10:13:50 +0200

    [FIX] - fixed test for mapping function

--- Response/src/test/kotlin/fr/sacane/response/functional/MapTest.kt
+++ Response/src/test/kotlin/fr/sacane/response/functional/MapTest.kt
@@ -2,37 +2,27 @@ package fr.sacane.response.functional
 
 import fr.sacane.response.Error
 import fr.sacane.response.Ok
-import fr.sacane.response.Response
 import fr.sacane.response.ok
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import fr.sacane.response.error
 
 class MapTest {
 
     @Test
-    fun `Map function should return null with null entry`(){
-        assertThrows<NullPointerException> {
-            ok<Int>().mapIfPresent { null }
-        }
-    }
-
-    @Test
     fun `Map function should transform correctly when status is ok`(){
-        val response = ok(1).mapIfPresent { it.toString() }
+        val response = ok<Int>().mapIfPresent({ it.toString() }, "2")
 
         assertTrue (
             response.status is Ok &&
             response.hasValue() &&
-            response.orElse(null) == "1"
+            response.orElse(null) == "2"
         )
     }
 
     @Test
     fun `Map function should not transform when status is error`(){
-        val response: Response<Int> = error<Int>("error").mapIfPresent { 5 }
-
+        val response = error<Int>("error").mapIfPresent({ "2" }, "2")
         assertTrue (
             response.status is Error &&
             !response.hasValue() &&
