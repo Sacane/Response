2bb8a736ad8025c3d122df40da3bd65d4a5e729a
Sacane <johan.ramaroson@gmail.com>
2023-07-21 11:07:51 +0200

    [CORE - Remove non safe use of isSuccess and isFailure methods

--- Response/src/main/kotlin/fr/sacane/response/Status.kt
+++ Response/src/main/kotlin/fr/sacane/response/Status.kt
@@ -6,32 +6,11 @@ sealed interface Status {
             is Ok -> "OK"
             is Error -> "ERROR"
         }
-    fun isSuccess(): Boolean = when(this) {
-        is Ok -> true
-        is Error -> false
-    }
-    fun isFailure(): Boolean = when(this) {
-        is Ok -> false
-        is Error -> true
-    }
 }
 
-private fun Status.check() {
-    if(!isSuccess() && !isFailure()) {
-        throw IllegalStateException("Status should return true for isSuccess or isFailure")
-    }
-}
 
-open class Ok: Status {
-    init {
-        check()
-    }
-}
+open class Ok: Status
 open class Error(val message: String): Status {
-    init {
-        check()
-    }
-
     override fun toString(): String {
         return "$symbol(message='$message')"
     }
