c561916af68d926436c57c589c87d0280aed0147
Sacane <johan.ramaroson@gmail.com>
2023-07-25 15:13:03 +0200

    [FIX] cleared empty factory http status

--- Response/src/main/kotlin/fr/sacane/response/http/HttpSuccess.kt
+++ Response/src/main/kotlin/fr/sacane/response/http/HttpSuccess.kt
@@ -1,6 +1,5 @@
 package fr.sacane.response.http
 
-import fr.sacane.response.EmptyOk
 import fr.sacane.response.Ok
 import fr.sacane.response.Response
 
@@ -10,14 +9,8 @@ sealed class HttpSuccess (val code: Int): Ok() {
         get() = createdSymbol
 }
 
-sealed class EmptyHttpSuccess (val code: Int): EmptyOk() {
-    override val symbol: String
-        get() = createdSymbol
-}
-
-class Created (val url: String): EmptyHttpSuccess(201)
-class CreatedWithBody internal constructor(val url: String): HttpSuccess(201)
+class Created (val url: String): HttpSuccess(201)
 
 fun created() = Response(null, Created(""))
 fun created(url: String) = Response(null, Created(url))
-fun <E> created(url: String, value: E) = Response(value, CreatedWithBody(url))
+fun <E> created(url: String, value: E) = Response(value, Created(url))
